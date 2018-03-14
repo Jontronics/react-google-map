2462691a65c6fafebf158a8c29a9a918dd83b1f9
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-03-13 22:44:57 -0400

    moved div className=sidebar-wrapper from Home.js into Search.js and everything still works

--- src/components/containers/Search.js
+++ src/components/containers/Search.js
@@ -9,19 +9,31 @@ class Search extends Component {
     }
   }
   
-  render(){
-    
+  render(){  
     const markers = [
       {id:1, key:'1', defaultAnimation:2, label: 'skate-board', position:{lat:40.7224017, lng:-73.9896719}},
       {id:2, key:'2', defaultAnimation:2, label: 'skate-board', position:{lat:40.7024017, lng:-73.9896719}},
     ]
-    
+
     return(
-      
-      
-      
-      
-      
+      <div className="sidebar-wrapper" style={{height:960}}>    
+          <Map 
+            onMapReady={ (map) => {
+              if (this.state.map != null)
+                  return 
+                  console.log('OnMapReady: '+JSON.stringify(map.getCenter()))
+                  this.setState({
+                      map: map
+                  })
+            }}  
+            
+            markers={markers}
+            zoom={14}
+            center={{lat:40.7224017, lng:-73.9896719}}
+            containerElement={<div style={{height:100+'%'}} />}
+            mapElement={<div style={{height:100+'%'}} />} />
+      </div>
+        
     )
   }
   
