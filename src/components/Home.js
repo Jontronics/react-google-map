bfc1791f7df59aeb4d25e0a6294cf498d67dea6b
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-03-13 21:17:07 -0400

    google maps working with hard coded pins

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,26 +9,31 @@ class Home extends Component {
     }
   }
     render(){
+      const markers = [
+        {id:1, key:'1', defaultAnimation:2, label: 'skate-board', position:{lat:40.7224017, lng:-73.9896719}},
+        {id:2, key:'2', defaultAnimation:2, label: 'skate-board', position:{lat:40.7024017, lng:-73.9896719}},
+
+      ]
+      
       return(
         <div className="wrapper">
             <div className="sidebar" data-background-color="white" data-active-color="danger">
-              <div className="sidebar-wrapper" style={{height:960}}>  
-                
-              <Map 
-                onMapReady={ (map) => {
-                  if (this.state.map != null)
-                      return 
-                      console.log('OnMapReady: '+JSON.stringify(map.getCenter()))
-                      this.setState({
-                          map: map
-                      })
-                }}  
-                
-                zoom={14}
-                center={{lat:40.7224017, lng:-73.9896719}}
-                containerElement={<div style={{height:100+'%'}} />}
-                mapElement={<div style={{height:100+'%'}} />} />
-
+              <div className="sidebar-wrapper" style={{height:960}}>    
+                  <Map 
+                    onMapReady={ (map) => {
+                      if (this.state.map != null)
+                          return 
+                          console.log('OnMapReady: '+JSON.stringify(map.getCenter()))
+                          this.setState({
+                              map: map
+                          })
+                    }}  
+                    
+                    markers={markers}
+                    zoom={14}
+                    center={{lat:40.7224017, lng:-73.9896719}}
+                    containerElement={<div style={{height:100+'%'}} />}
+                    mapElement={<div style={{height:100+'%'}} />} />
               </div>
             </div>
             <div className="main-panel">
--- src/components/presentation/index.js
+++ src/components/presentation/index.js
@@ -8,6 +8,7 @@ export {
   
   Nav,
   Footer,
-  Item
+  Item,
+  Map
   
 }
