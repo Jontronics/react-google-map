5517e001db02e4cee6962b5d1856493c3993369b
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-03-20 11:47:10 -0400

    bug fixed. darn bracket

--- src/components/containers/Results.js
+++ src/components/containers/Results.js
@@ -8,7 +8,7 @@ class Results extends Component {
     super()
     this.state = {
       item: {
-        position: {lat:40.70224017, lng:-73.9796719}}
+        position: {lat:40.70224017, lng:-73.9796719} 
       }
     }
   }
@@ -25,14 +25,9 @@ updateItem(attr, event){
 
 addItem(){
   console.log('ADD ITEM: ' + JSON.stringify(this.state.item))
-  
-  let newItem = Object.assign({}, this.state.item)
-  
-  newItem['id'] = 100
-  newItem['key'] = '100'
-  newItem['defaultAnimation'] = 2
 
-  this.props.addItem(newItem)
+
+  this.props.addItem(this.state.item)
 }
   
   render(){
